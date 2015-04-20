6b4cb3c452ae1648938dfe0651c1fb265eff2083
ballchen <kerkerball@gmail.com>
2015-04-20 22:54:18 +0800

    change channel

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var Record = require('./models').record;
 
 
 var fb = require('./utils/fb/fb');
-var hooker = '1562916150625754';
-// var hooker = '1430308183926745';
+// var hooker = '1562916150625754';
+var hooker = '1430308183926745';
 
 
 fb.login(function(err, fbuser) {
