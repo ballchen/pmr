1a2ebee91db455713e2f99396d7d8b21059b6c8a
ballchen <kerkerball@gmail.com>
2015-05-19 15:11:28 +0800

    add wootalk

--- utils/fb/fb.js
+++ utils/fb/fb.js
@@ -8,19 +8,52 @@ var secret = require('./secret.js');
 var CronJob = require('cron').CronJob;
 var commands = require('./commands').commands;
 var Cleverbot = require('cleverbot-node');
+var async = require('async');
+var request = require('request');
+var _ = require('underscore');
+var wootalk_url = 'https://wootalk.today/';
+var WebSocket = require('ws');
+var ent = require('ent');
+
+var wootalk_header = {
+	'Host': 'wootalk.today',
+	'Origin': 'https://wootalk.today',
+	'Cache-Control': 'no-cache',
+	'Pragma': 'no-cache',
+	'Connection': 'Upgrade',
+	'Upgrade': 'websocket',
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36',
+	'Sec-WebSocket-Version': 13,
+	'Accept-Encoding': 'gzip, deflate, sdch',
+	'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-CN;q=0.2'
+};
+var Randomize = function() {
+	return Math.floor((Math.random() * 100000) + 50000);
+};
+
+var leavecmd = '["change_person",{}]';
 
 
 
 //hello
 var message = 'https://www.facebook.com/ajax/mercury/send_messages.php';
-var friends = 'https://www.facebook.com/friends'
+var friends = 'https://www.facebook.com/friends';
 
 var users = {
 	'甲豪': '100000187207997',
 	'小馬': '100000032300808'
 };
 var lonely = false;
+var wootalk_spy_on = false;
+var wootalk_spy_timeout = false;
+
+var wootalk_chat = '';
+var userId_A = null;
+
 var bot = new Cleverbot;
+var wsA;
+var wsB;
+var wsChat;
 
 
 //initial a cookie jar to save the session
@@ -116,9 +149,15 @@ exports.get_messages = function get_messages(seq, callback) {
 		timeout: 60000
 	}, function(err, httpResponse, body) {
 		var cuthead = /for \(;;\); (.+)/
+		var raw;
 
-		var raw = JSON.parse(cuthead.exec(body)[1])
-			// console.log(raw.seq);
+		try {
+			raw = JSON.parse(cuthead.exec(body)[1])
+		} catch (error) {
+			raw = {};
+		}
+
+		// console.log(raw.seq);
 		if (raw.ms) {
 			_.each(raw.ms, function(elem) {
 				if (elem.type == 'm_messaging' && elem.event != 'read' && elem.author_fbid != fb_userid) {
@@ -134,6 +173,33 @@ exports.get_messages = function get_messages(seq, callback) {
 							} else if (elem.message == 'spy off') {
 								spy = false;
 								send_messages(elem.author_fbid, null, '竊聽模式已關閉');
+							}
+
+							if (elem.message == '想聽相聲') {
+								if (!wootalk_spy_on) {
+									wootalk_spy_on = true;
+									console.log('準備中...');
+									send_messages(elem.author_fbid, null, '準備中...');
+									wootalk_spy(elem.author_fbid);
+								} else {
+									send_messages(elem.author_fbid, null, '< 這場表演尚未結束 >');
+								}
+
+							}
+							if (elem.message == '滾') {
+								if (wootalk_spy_on) {
+									wsA.close();
+									wsB.close();
+									send_messages(elem.author_fbid, null, 'A、B：「 客倌別生氣，我們這就滾 ^ ^" 」');
+								} else {
+									send_messages(elem.author_fbid, null, '兇屁兇？幹！');
+								}
+
+							}
+
+							if (elem.message == '想要妹子') {
+								send_messages(elem.author_fbid, null, '我來了！');
+
 							}
 
 						}
@@ -155,8 +221,8 @@ exports.get_messages = function get_messages(seq, callback) {
 					else if (elem.tid.substr(0, 3) == 'id.') {
 						var thread_fbid = elem.tid.split(/id\.(.+)/)[1]
 						console.log('[群組] ' + elem.thread_name + "(" + thread_fbid + "): " + elem.message)
-							// if (thread_fbid == '1430308183926745') {
-						if (thread_fbid == '1562916150625754') {
+						if (thread_fbid == '1430308183926745') {
+							// if (thread_fbid == '1562916150625754') {
 							if (lonely && elem.message == '我好多了') {
 								lonely = false;
 								send_messages(null, thread_fbid, '^ _ ^');
@@ -188,6 +254,44 @@ exports.get_messages = function get_messages(seq, callback) {
 								})
 							}
 
+							// Wootalk Spy
+							// 
+							if (elem.message == '想聽相聲' || elem.message == '聽相聲') {
+								if (!wootalk_spy_on) {
+									wootalk_spy_on = true;
+									console.log('準備中...');
+									send_messages(null, thread_fbid, '準備中...');
+									wootalk_spy(null, thread_fbid);
+								} else {
+									send_messages(null, thread_fbid, '< 這場表演尚未結束 >');
+								}
+
+							}
+							if (elem.message == '滾') {
+								if (wootalk_spy_on) {
+									wsA.close();
+									wsB.close();
+									send_messages(null, thread_fbid, 'A、B：「 客倌別生氣，我們這就滾 ^ ^" 」');
+								} else {
+									send_messages(null, thread_fbid, '兇屁兇？幹！');
+								}
+
+							}
+
+							// wootalk chat
+							if (elem.message == '想要妹子') {
+								typing(null, thread_fbid);
+								console.log('hi');
+								//set who's chatter
+
+								wootalk_chat = elem.author_fbid;
+								wootalk_chat_fc(null, thread_fbid);
+
+
+							}
+							if (wootalk_chat && (elem.author_fbid == wootalk_chat)) {
+								wootalk_chat_send(elem.message);
+							}
 
 
 						}
@@ -288,6 +392,33 @@ function send_messages(receiver, tid, text, sticker) {
 	})
 }
 
+function typing(to, thread) {
+	if (!to) {
+		to = '';
+	}
+	if (!thread) {
+		thread = to;
+	}
+	data = {
+		"typ": "1",
+		"to": to,
+		"source": "web-messenger",
+		"thread": thread,
+		"__user": fb_userid,
+		"__a": "1",
+		"__dyn": "7n8anEBQ9FoBUSt2u6aAix97xN6yUgByV9GiyFqzQC-C26m6oDAyoSnx2ubhHAyXBBzEy5E",
+		"__req": "c",
+		"fb_dtsg": fb_dtsg,
+		"ttstamp": "26581691011017411284781047297",
+		"__rev": "1436610",
+	};
+	fbrequest({
+		method: 'POST',
+		url: 'https://www.facebook.com/ajax/messaging/typ.php',
+		form: data
+	});
+}
+
 exports.send_messages = send_messages;
 
 var search_user = function(fbid, ids, callback) {
@@ -318,6 +449,8 @@ exports.get_buddy = function(fbid) {
 		console.log(body)
 	})
 }
+
+
 
 exports.get_some_friends = function(fbid) {
 
@@ -369,4 +502,319 @@ function ascii2native(input) {
 		}
 	};
 	return native;
+}
+
+var wootalk_chat = function(reciever, tid) {
+	request.get(wootalk_url, function(error, response, body) {
+		var session = response.headers['set-cookie'][0].match(/_wootalk_session=(\w+)/)[1];
+
+	});
+}
+var w_spy_timeout = function(receiver, tid) {
+	setTimeout(function() {
+		if (wootalk_spy_timeout) {
+			send_messages(null, tid, '雙方含情脈脈，此刻的情景無法用言語表達。');
+		}
+	}, 10000)
+}
+var wootalk_spy = function(receiver, tid) {
+	async.parallel([
+		function(cb) {
+			request.get(wootalk_url, function(error, response, body) {
+				var session = response.headers['set-cookie'][0].match(/_wootalk_session=(\w+)/)[1];
+				cb(null, session);
+			});
+		},
+		function(cb) {
+			request.get(wootalk_url, function(error, response, body) {
+				var session = response.headers['set-cookie'][0].match(/_wootalk_session=(\w+)/)[1];
+				cb(null, session);
+			});
+		}
+	], function(err, results) {
+		wsA = new WebSocket('wss://wootalk.today/websocket', [], {
+			headers: _.extend(wootalk_header, {
+				'Cookie': '_gat=1; _wootalk_session=' + results[0] + '; _ga=GA1.2.1804571589.1429605824; __asc=6c4424fc14ce5fe7639ea11437a; __auc=c71404c914cdb259f913b23fc5b'
+			})
+		});
+		wsB = new WebSocket('wss://wootalk.today/websocket', [], {
+			headers: _.extend(wootalk_header, {
+				'Cookie': '_gat=1; _wootalk_session=' + results[1] + '; _ga=GA1.2.1804571589.1429605824; __asc=6c4424fc14ce5fe7639ea11437a; __auc=c71404c914cdb259f913b23fc5b'
+			})
+		});
+
+
+
+		wsA.on('open', function() {
+			// console.log('A connected!');
+			wootalk_spy_timeout = true;
+			w_spy_timeout(receiver, tid)
+		});
+
+
+		wsA.on('message', function(message) {
+
+			// console.log(message)
+			var pa = JSON.parse(message)[0]; //parse
+			var ev = pa[0]; //event名字
+			/*
+				client_connected, new_message, websocket_rails.ping, update_state
+			*/
+			if (pa[1]['data']['message']) { //avoid undefined
+				pa[1]['data']['message'] = ent.decode(pa[1]['data']['message']); //html entity
+			}
+			var msg = pa[1]['data']['message'];
+			var sender = pa[1]['data']['sender']; //0 是系統, 1是自己, 2是對方
+			var leave = pa[1]['data']['leave']; //若對方leave, 要寄給系統["change_person",{}]
+			if (ev == 'new_message') {
+				pa[1]['data']['sender'] = 1; //新增這行
+				message = JSON.stringify(pa); //新增這行
+				// console.log(message)
+				if (sender == 2) {
+					wootalk_spy_timeout = false;
+					console.log("A：「 " + msg + " 」")
+					send_messages(null, tid, "A：「 " + msg + " 」");
+					wsB.send(message);
+
+				} else if (!sender && leave) {
+					//leave == false 是初始系統提示訊息的時候, 其餘時候都是undefined
+					//change person 或 disconnected
+					//
+					console.log('A 已經離開房間');
+					send_messages(null, tid, 'A 匆匆離去');
+
+					wsA.close();
+					wsB.close();
+				}
+			} else if (ev == 'update_state') {
+
+				if (pa[1]['data']['typing']) {
+					//console.log('A typing...');
+					typing(receiver, tid);
+				}
+				if (pa[1]['data']['last_read']) {
+					//console.log('A 已讀');
+				}
+				wsB.send(message);
+
+
+			} else if (ev == 'websocket_rails.ping') {
+				a = Randomize();
+				wsA.send('["websocket_rails.pong",{"id":' + Randomize() + ',"data":{}}]')
+			} else if (ev == 'client_connected') {
+				console.log('A 已進入房間')
+				send_messages(null, tid, 'A 上台一鞠躬');
+
+			}
+		});
+
+		wsA.on('close', function() {
+			console.log('A disconnected');
+			send_messages(null, tid, 'A 下台一鞠躬');
+			wootalk_spy_on = false;
+		});
+
+
+
+		wsB.on('open', function() {
+			// console.log('B connected!');
+
+		});
+		wsB.on('message', function(message) {
+			var pa = JSON.parse(message)[0]; //parse
+			var ev = pa[0]; //event名字
+
+			/*
+				client_connected, new_message, websocket_rails.ping, update_state
+			*/
+
+			if (pa[1]['data']['message']) { //avoid undefined
+				pa[1]['data']['message'] = ent.decode(pa[1]['data']['message']); //html entity
+			}
+			var msg = pa[1]['data']['message'];
+			var sender = pa[1]['data']['sender']; //0 是系統, 1是自己, 2是對方
+			var leave = pa[1]['data']['leave']; //若對方leave, 要寄給系統["change_person",{}]
+			if (ev == 'new_message') {
+				wootalk_spy_timeout = false;
+				pa[1]['data']['sender'] = 1; //新增這行
+				message = JSON.stringify(pa); //新增這行
+				// console.log(message)
+				if (sender == 2) {
+					console.log("B：「 " + msg + " 」")
+					send_messages(null, tid, "B：「 " + msg + " 」");
+					wsA.send(message);
+				} else if (!sender && leave) {
+					//leave == false 是初始系統提示訊息的時候, 其餘時候都是undefined
+					//change person 或 disconnected
+					console.log('B 已經離開房間');
+					send_messages(null, tid, 'B 匆匆離去');
+
+
+					wsA.close();
+					wsB.close();
+				}
+			} else if (ev == 'update_state') {
+				if (pa[1]['data']['typing']) {
+					//console.log('B typing...');
+					typing(receiver, tid);
+				}
+				if (pa[1]['data']['last_read']) {
+					//console.log('B 已讀');
+				}
+				wsA.send(message);
+
+
+			} else if (ev == 'websocket_rails.ping') {
+				a = Randomize();
+				wsB.send('["websocket_rails.pong",{"id":' + Randomize() + ',"data":{}}]')
+
+			} else if (ev == 'client_connected') {
+				console.log('B 已進入房間');
+				send_messages(null, tid, 'B 上台一鞠躬');
+				// wsB.send('["new_message",{"id":70527,"data":{"message":"嗨","msg_id":1}}]')
+			}
+		});
+
+		wsB.on('close', function() {
+			console.log('B disconnected');
+			send_messages(null, tid, 'B 下台一鞠躬');
+			wootalk_spy_on = false;
+		});
+
+	});
+};
+
+var wootalk_chat_fc = function(receiver, tid) {
+	async.parallel([
+		function(cb) {
+			request.get(wootalk_url, function(error, response, body) {
+				var session = response.headers['set-cookie'][0].match(/_wootalk_session=(\w+)/)[1];
+				cb(null, session);
+			});
+		}
+	], function(err, results) {
+		wsChat = new WebSocket('wss://wootalk.today/websocket', [], {
+			headers: _.extend(wootalk_header, {
+				'Cookie': '_gat=1; _wootalk_session=' + results[0] + '; _ga=GA1.2.1804571589.1429605824; __asc=6c4424fc14ce5fe7639ea11437a; __auc=c71404c914cdb259f913b23fc5b'
+			})
+		});
+
+
+		wsChat.on('open', function() {
+			// console.log('A connected!');
+
+		});
+
+
+		wsChat.on('message', function(message) {
+			//console.log(message)
+			var pa = JSON.parse(message)[0]; //parse
+			var ev = pa[0]; //event名字
+			/*
+				client_connected, new_message, websocket_rails.ping, update_state
+			*/
+			if (pa[1]['data']['message']) {
+				pa[1]['data']['message'] = ent.decode(pa[1]['data']['message']); //html entity
+			}
+			var msg = pa[1]['data']['message'];
+			var sender = pa[1]['data']['sender']; //0 是系統, 1是自己, 2是對方
+			var leave = pa[1]['data']['leave']; //若對方leave, 要寄給系統["change_person",{}]
+			//console.log(pa[1]['data']['message']);
+			if (ev == 'new_message') {
+				//console.log(message)
+				if (pa[1]['user_id']) {
+					userId_A = pa[1]['user_id'];
+
+					//console.log(message);
+				}
+				pa[1]['data']['sender'] = 1; //使系統知道是我要傳給對方
+				message = JSON.stringify(pa);
+				if (sender == 2) {
+					//fakePaToB = pa;//取得傳假話參數");
+					console.log("A：「 " + msg + " 」");
+					send_messages(receiver, tid, msg);
+					//console.log(message);
+					//wsB.send(message);
+				} else if (!sender && leave) {
+					//leave == false 是初始系統提示訊息的時候, 其餘時候都是undefined
+					//change person 或 disconnected
+					//
+					console.log('A 已經離開房間');
+					send_messages(receiver, tid, '洗澡，掰掰～');
+
+
+					wsChat.send(leavecmd);
+					wootalk_chat = '';
+					userId_A = null;
+				}
+			} else if (ev == 'update_state') {
+
+				if (pa[1]['data']['typing']) {
+					typing(receiver, tid)
+				}
+				if (pa[1]['data']['last_read']) {
+					//console.log('A 已讀');
+				}
+
+
+			} else if (ev == 'websocket_rails.ping') {
+				a = Randomize();
+				wsChat.send('["websocket_rails.pong",{"id":' + Randomize() + ',"data":{}}]')
+			} else if (ev == 'client_connected') {
+				send_messages(receiver, tid, '我來了！');
+
+			}
+		});
+
+		wsChat.on('close', function() {
+			console.log('A disconnected');
+			send_messages(receiver, tid, '掰掰');
+			userId_A = null;
+		});
+
+	});
+}
+
+var wootalk_chat_send = function(content) {
+
+	//var temp;
+	var fakeMessage;
+	//var sample = ["new_message",{"id":null,"channel":null,"user_id":845609,"data":{"sender":1,"message":"å®å®","time":1431750704164,"msg_id":1},"success":null,"result":null,"token":null,"server_token":null}];
+	var sample = ["new_message", {
+		"id": null,
+		"channel": null,
+		"user_id": 876225,
+		"data": {
+			"sender": 1,
+			"message": "對",
+			"time": 1431952068796,
+			"mobile": null
+		},
+		"success": null,
+		"result": null,
+		"token": null,
+		"server_token": null
+	}];
+	// if (input) {
+	// 	//var sendToWho = input.substring(0, 3);
+	// 	//var content =  input.substring(4, input.length-1);
+	// 	var content = input.substring(0, input.length - 1);
+	// }
+	if (content == '88') { //sendToWho
+		wsChat.send(leavecmd);
+		wsChat.close();
+		userId_A = null;
+	} else if (userId_A) { //sendToWho == 'toa' &&
+		//console.log('發話給A用的user_id: '+userId_A);
+		//console.log('代替輸入send to A: '+content);
+		//temp = fakePaToA;
+		sample[1]['user_id'] = userId_A;
+		sample[1]['data']['message'] = content;
+		//temp[1]['data']['message'] = content;
+		fakeMessage = JSON.stringify(sample);
+		//console.log(fakeMessage);
+		wsChat.send(fakeMessage);
+	} else {
+		console.log('尚未取得足夠對話參數')
+	}
 }
